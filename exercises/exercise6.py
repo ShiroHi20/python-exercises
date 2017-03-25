d3448adac0bdac1f3ae49a4d25541313e157c21e
Sabrina Bernardelli <sabrinabernardelli@outlook.es>
2017-03-24 23:46:14 -0300

    Refactor

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -2,38 +2,22 @@
 #Dada una lista de numeros,mostrar el más grande, el más pequeño y el promedio.
 
 def buscarMenor(lista):
-    i = 0 
-    menor = lista[0]
-    while i < len(lista): 
-        if lista[i] < menor:
-            menor = lista[i]
-        i = i + 1 
-    return menor 
-
+    return min(lista)
 
 def buscarMayor(lista):
-    e = 0 
-    mayor = lista[0]
-    while e < len(lista):
-        if lista[e] > mayor:
-            mayor = lista[e]
-        e = e +1
-    return mayor 
-    
+    return max(lista)   
     
 def sacarPromedio(lista):
-   return sum(lista) / len(lista) 
-   
+    return sum(lista) / len(lista) 
 
-def main(listita):
+def main(listita):     
     menor = buscarMenor(listita)
     mayor = buscarMayor(listita)
     promedio = sacarPromedio(listita)
-    print ("el numero menor es: {}, \
-    el mayor es: {}, \
-    el promedio es: {}".format(menor,mayor,promedio))
-    
+
+    print ("el numero menor es: {}\n \
+    el mayor es: {}\n \
+    el promedio es: {}".format(menor, mayor, promedio))
     
 listita = [1,45,67,87,90,21,4,55,22,564]
-
 main(listita)
